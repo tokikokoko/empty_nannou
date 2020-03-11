805b1678d3033c06dd926a72fea12b2650216f8c
tkk <tkk@gmail.com>
2020-03-11 14:48:29 +0900

    Add nannou

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use nannou::prelude::*;
+
+struct Model {}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello nannou!");
+}
+
+fn model(_app: &App) -> Model {
+    Model {}
+}
+
+fn event(_app: &App, _model: &mut Model, _event: Event) {
+}
+
+fn view(app: &App, _model: &Model, frame: Frame) {
 }
